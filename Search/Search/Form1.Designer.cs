408f8d5a13072b1696e8030ecb434b083eefde68
SinLok <chuiwork001@gmail.com>
2015-09-26 11:34:12 +0800

    Test

--- Search/Search/Form1.Designer.cs
+++ Search/Search/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Search
 {
-    partial class Form1
+    partial class SearchForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,132 +29,275 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cboWeekday = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtPrice2 = new System.Windows.Forms.TextBox();
+            this.txtPrice1 = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.nudQuota = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboMonth = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lstCourseDetail = new System.Windows.Forms.ListView();
+            this.Category = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CourseName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Weekday = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.StartMonth = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.EndMonth = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.StartTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.EndTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Quota = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Room = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Price = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuota)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 72);
+            this.label1.Location = new System.Drawing.Point(47, 67);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(52, 12);
             this.label1.TabIndex = 0;
             this.label1.Text = "Weekday:";
             // 
-            // label2
+            // cboWeekday
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(290, 76);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(11, 12);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "$";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(105, 69);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 20);
-            this.comboBox1.TabIndex = 3;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(307, 70);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(82, 22);
-            this.textBox1.TabIndex = 4;
+            this.cboWeekday.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboWeekday.FormattingEnabled = true;
+            this.cboWeekday.Items.AddRange(new object[] {
+            "",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"});
+            this.cboWeekday.Location = new System.Drawing.Point(105, 64);
+            this.cboWeekday.Name = "cboWeekday";
+            this.cboWeekday.Size = new System.Drawing.Size(157, 20);
+            this.cboWeekday.TabIndex = 3;
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.txtPrice2);
+            this.groupBox1.Controls.Add(this.txtPrice1);
+            this.groupBox1.Controls.Add(this.btnReset);
+            this.groupBox1.Controls.Add(this.btnSearch);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.numericUpDown1);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.nudQuota);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.cboMonth);
             this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Location = new System.Drawing.Point(102, 166);
+            this.groupBox1.Controls.Add(this.cboWeekday);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(640, 210);
+            this.groupBox1.Size = new System.Drawing.Size(369, 169);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Course Filter";
             // 
+            // txtPrice2
+            // 
+            this.txtPrice2.Location = new System.Drawing.Point(195, 97);
+            this.txtPrice2.Name = "txtPrice2";
+            this.txtPrice2.Size = new System.Drawing.Size(67, 22);
+            this.txtPrice2.TabIndex = 14;
+            // 
+            // txtPrice1
+            // 
+            this.txtPrice1.Location = new System.Drawing.Point(105, 97);
+            this.txtPrice1.Name = "txtPrice1";
+            this.txtPrice1.Size = new System.Drawing.Size(67, 22);
+            this.txtPrice1.TabIndex = 7;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(282, 96);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 50);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(282, 27);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 52);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(287, 37);
+            this.label6.Location = new System.Drawing.Point(18, 134);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(81, 12);
             this.label6.TabIndex = 10;
             this.label6.Text = "Available quota:";
             // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(374, 33);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(67, 22);
-            this.numericUpDown1.TabIndex = 11;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(105, 32);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 20);
-            this.comboBox2.TabIndex = 8;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(60, 35);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(39, 12);
-            this.label5.TabIndex = 7;
-            this.label5.Text = "Month:";
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(395, 74);
+            this.label4.Location = new System.Drawing.Point(178, 100);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(11, 12);
             this.label4.TabIndex = 6;
             this.label4.Text = "~";
             // 
-            // textBox2
+            // nudQuota
             // 
-            this.textBox2.Location = new System.Drawing.Point(412, 69);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(93, 22);
-            this.textBox2.TabIndex = 5;
+            this.nudQuota.Location = new System.Drawing.Point(105, 130);
+            this.nudQuota.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudQuota.Name = "nudQuota";
+            this.nudQuota.Size = new System.Drawing.Size(67, 22);
+            this.nudQuota.TabIndex = 11;
             // 
-            // Form1
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(11, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "$";
+            // 
+            // cboMonth
+            // 
+            this.cboMonth.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMonth.FormattingEnabled = true;
+            this.cboMonth.Items.AddRange(new object[] {
+            "",
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December "});
+            this.cboMonth.Location = new System.Drawing.Point(105, 27);
+            this.cboMonth.Name = "cboMonth";
+            this.cboMonth.Size = new System.Drawing.Size(157, 20);
+            this.cboMonth.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Month:";
+            // 
+            // lstCourseDetail
+            // 
+            this.lstCourseDetail.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Category,
+            this.CourseName,
+            this.Weekday,
+            this.StartMonth,
+            this.EndMonth,
+            this.StartTime,
+            this.EndTime,
+            this.Price,
+            this.Quota,
+            this.Room});
+            this.lstCourseDetail.Location = new System.Drawing.Point(12, 187);
+            this.lstCourseDetail.Name = "lstCourseDetail";
+            this.lstCourseDetail.Size = new System.Drawing.Size(720, 433);
+            this.lstCourseDetail.TabIndex = 6;
+            this.lstCourseDetail.UseCompatibleStateImageBehavior = false;
+            this.lstCourseDetail.View = System.Windows.Forms.View.Details;
+            // 
+            // Category
+            // 
+            this.Category.Text = "Category";
+            this.Category.Width = 71;
+            // 
+            // CourseName
+            // 
+            this.CourseName.Text = "Course Name";
+            this.CourseName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.CourseName.Width = 103;
+            // 
+            // Weekday
+            // 
+            this.Weekday.Text = "Weekday";
+            this.Weekday.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Weekday.Width = 66;
+            // 
+            // StartMonth
+            // 
+            this.StartMonth.Text = "Start Month";
+            this.StartMonth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.StartMonth.Width = 74;
+            // 
+            // EndMonth
+            // 
+            this.EndMonth.Text = "End Month";
+            this.EndMonth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.EndMonth.Width = 67;
+            // 
+            // StartTime
+            // 
+            this.StartTime.Text = "Start Time";
+            this.StartTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.StartTime.Width = 65;
+            // 
+            // EndTime
+            // 
+            this.EndTime.Text = "End Time";
+            this.EndTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.EndTime.Width = 63;
+            // 
+            // Quota
+            // 
+            this.Quota.Text = "Quota";
+            this.Quota.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Quota.Width = 53;
+            // 
+            // Room
+            // 
+            this.Room.Text = "Room";
+            this.Room.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Price
+            // 
+            this.Price.Text = "Price";
+            this.Price.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // SearchForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(926, 483);
+            this.ClientSize = new System.Drawing.Size(746, 633);
+            this.Controls.Add(this.lstCourseDetail);
             this.Controls.Add(this.groupBox1);
-            this.Name = "Form1";
+            this.Name = "SearchForm";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudQuota)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -162,16 +305,29 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cboWeekday;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox cboMonth;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown nudQuota;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListView lstCourseDetail;
+        private System.Windows.Forms.ColumnHeader Category;
+        private System.Windows.Forms.ColumnHeader CourseName;
+        private System.Windows.Forms.ColumnHeader Weekday;
+        private System.Windows.Forms.ColumnHeader StartMonth;
+        private System.Windows.Forms.ColumnHeader EndMonth;
+        private System.Windows.Forms.ColumnHeader StartTime;
+        private System.Windows.Forms.ColumnHeader EndTime;
+        private System.Windows.Forms.ColumnHeader Quota;
+        private System.Windows.Forms.ColumnHeader Room;
+        private System.Windows.Forms.TextBox txtPrice2;
+        private System.Windows.Forms.TextBox txtPrice1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColumnHeader Price;
     }
 }
 
--- Search/Search/Form1.cs
+++ Search/Search/Form1.cs
@@ -7,14 +7,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace Search
 {
-    public partial class Form1 : Form
+    public partial class SearchForm : Form
     {
-        public Form1()
+        OleDbConnection connection;
+        OleDbDataAdapter dataAdapter;
+        DataTable dt;
+        string showAllCourse;
+        double profitMargin;
+        string connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=masterDB.accdb";
+
+        public SearchForm()
         {
             InitializeComponent();
+            connection = new OleDbConnection(connectionString);
+            dt = new DataTable();
+            string sql = "SELECT percentage FROM profitMargin";
+            dataAdapter = new OleDbDataAdapter(sql, connection);
+            dataAdapter.Fill(dt);
+            profitMargin = Convert.ToDouble(dt.Rows[0]["percentage"].ToString());
+
+            dt.Clear();
+            showAllCourse = "SELECT cc.name, c.name, weekday, startMonth, endMonth, startTime, endTime, ((teacherRate + operatingCharges) * "+
+              profitMargin + ") / 10 as Price, quota, room FROM course c, courseCategory cc WHERE c.categoryID = cc.categoryID";
+            dataAdapter.SelectCommand.CommandText = showAllCourse;
+            dataAdapter.Fill(dt);
+            FillListView();
+        }
+        private void FillListView()
+        {
+            lstCourseDetail.Items.Clear();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                lstCourseDetail.Items.Add(dt.Rows[i]["cc.name"].ToString());
+                lstCourseDetail.Items[i].SubItems.Add(dt.Rows[i]["c.name"].ToString());
+                lstCourseDetail.Items[i].SubItems.Add(dt.Rows[i]["weekday"].ToString());
+                lstCourseDetail.Items[i].SubItems.Add(dt.Rows[i]["startMonth"].ToString());
+                lstCourseDetail.Items[i].SubItems.Add(dt.Rows[i]["endMonth"].ToString());
+                lstCourseDetail.Items[i].SubItems.Add(dt.Rows[i]["startTime"].ToString());
+                lstCourseDetail.Items[i].SubItems.Add(dt.Rows[i]["endTime"].ToString());
+                lstCourseDetail.Items[i].SubItems.Add(dt.Rows[i]["Price"].ToString());
+                lstCourseDetail.Items[i].SubItems.Add(dt.Rows[i]["quota"].ToString());
+                lstCourseDetail.Items[i].SubItems.Add(dt.Rows[i]["room"].ToString());
+            }
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string selectString = "SELECT cc.name, c.name, weekday, startMonth, endMonth, startTime, endTime,  ((teacherRate + operatingCharges) * " +
+              profitMargin + ") / 10 as Price, quota, room FROM course c, courseCategory cc WHERE c.categoryID = cc.categoryID"
+                + " AND quota >= " + Convert.ToInt32(nudQuota.Value.ToString());
+
+            if (cboMonth.SelectedIndex != 0)
+                selectString += " AND " + cboMonth.SelectedIndex + " BETWEEN startMonth AND endMonth";
+
+            if (cboWeekday.SelectedIndex != 0)
+                selectString += " AND weekday =" + cboWeekday.SelectedIndex;
+
+            if(!(txtPrice1.Text.Equals("") || txtPrice2.Text.Equals("")))
+                selectString += " AND ((teacherRate + operatingCharges) * " + profitMargin + ") / 10 BETWEEN " + txtPrice1.Text + " AND " + txtPrice2.Text;
+
+            dataAdapter.SelectCommand.CommandText = selectString;
+            dt.Clear();
+            dataAdapter.Fill(dt);
+            FillListView();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            cboMonth.SelectedIndex = 0;
+            cboWeekday.SelectedIndex = 0;
+            txtPrice1.Text = "";
+            txtPrice2.Text = "";
+            nudQuota.Value = 0;
+
+            dataAdapter.SelectCommand.CommandText = showAllCourse;
+            dt.Clear();
+            dataAdapter.Fill(dt);
+            FillListView();
         }
     }
 }
